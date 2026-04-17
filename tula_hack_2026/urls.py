bdc8c1f8212cbbcaab16b9ffa80e78dc741b4455
vsevolod.rachis <seva-ra4is@mail.ru>
2026-04-18 03:48:05 +0700

    add index.html template

--- tula_hack_2026/urls.py
+++ tula_hack_2026/urls.py
@@ -1,5 +1,11 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.conf.urls.static import static
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("core.urls")), path("users/", include("users.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("core.urls")),
+    path("users/", include("users.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
