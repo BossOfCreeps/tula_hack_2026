3a31faf9c881e6e0f598f5b3762c1aa90e1d60da
vsevolod.rachis <seva-ra4is@mail.ru>
2026-04-18 05:45:06 +0700

    кнопки team_detail

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from core.views import TeamDetailView
+from core.views import TeamDetailView, TeamUserRemoveView, TeamUserAddView
 
 urlpatterns = [
-    path("team/<int:pk>/", TeamDetailView.as_view(), name="index"),
+    path("team/<int:pk>/", TeamDetailView.as_view(), name="team-detail"),
+    path("team_user/<int:team_id>/<int:user_id>/add", TeamUserAddView.as_view(), name="team_user-add"),
+    path("team_user/<int:team_id>/<int:user_id>/remove", TeamUserRemoveView.as_view(), name="team_user-remove"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import DetailView
+from django.http import HttpResponseRedirect
+from django.views.generic import DetailView, View
 
 from core.models import Team
 from users.models import User
@@ -9,3 +10,15 @@ class TeamDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         return super().get_context_data(**kwargs) | {"all_users": User.objects.all()}
+
+
+class TeamUserAddView(View):
+    def get(self, request, team_id, user_id):
+        Team.objects.get(id=team_id).users.add(User.objects.get(id=user_id))
+        return HttpResponseRedirect(f"/team/{team_id}/")
+
+
+class TeamUserRemoveView(View):
+    def get(self, request, team_id, user_id):
+        Team.objects.get(id=team_id).users.remove(User.objects.get(id=user_id))
+        return HttpResponseRedirect(f"/team/{team_id}/")
